ad743e321e5dca91addd20012d97eecd5a58f35d
wukat <wojciechksprk@gmail.com>
2014-03-29 14:26:16 +0100

    graph visualisation changed to returnig layouts

--- src/graph/GraphVisualisation.java
+++ src/graph/GraphVisualisation.java
@@ -1,22 +1,17 @@
 package graph;
 
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.FRLayout;
 import edu.uci.ics.jung.algorithms.layout.ISOMLayout;
 import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.algorithms.layout.SpringLayout;
-import edu.uci.ics.jung.visualization.VisualizationViewer;
 
 /**
  * @author Krzysztof Spytkowski
  * @date 24 mar 2014
  */
-public class GraphVisualisation extends JFrame {
+public class GraphVisualisation {
 	
 	private static final long serialVersionUID = 1L; // default serial version number
 	private final MyGraph graph; // graph to be displayed
@@ -36,52 +31,58 @@ public class GraphVisualisation extends JFrame {
 	 * @param l
 	 *            - layout
 	 */
-	private void visualize(Layout<Integer, String> l) {
-		setSize(new Dimension(400, 300));
-		VisualizationViewer<Integer, String> vv = new VisualizationViewer<>(l, new Dimension(300, 200));
-		getContentPane().add(vv);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
-		setVisible(true);
-	};
+//	private void visualize(Layout<Integer, String> l) {
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		VisualizationViewer<Integer, String> vv = new VisualizationViewer<>(l, new Dimension(300, 200));
+//                vv.setGraphMouse(new DefaultModalGraphMouse<String, Number>());
+//		getContentPane().add(vv);
+//		pack();
+//		setSize(new Dimension(400, 300));
+//		setVisible(true);
+//	};
 
 	/**
 	 * Visualize graph (A simple layout places vertices randomly on a circle)
+         * @return layout
 	 */
-	public void CircleGraphVisualisation() {
+	public Layout<Integer, String> CircleGraphVisualisation() {
 		Layout<Integer, String> layout = new CircleLayout<>(graph.getGraph());
-		visualize(layout);
+                return layout;
 	}
 
 	/**
 	 * Visualize graph (The Kamada-Kawai algorithm for node layout)
+         * @return layout
 	 */
-	public void KKGraphVisualisation() {
+	public Layout<Integer, String> KKGraphVisualisation() {
 		Layout<Integer, String> layout = new KKLayout<>(graph.getGraph());
-		visualize(layout);
+		return layout;
 	}
 
 	/**
 	 * Visualize graph (The Fruchterman-Rheingold algorithm)
+         * @return layout
 	 */
-	public void FRGraphVisualisation() {
+	public Layout<Integer, String> FRGraphVisualisation() {
 		Layout<Integer, String> layout = new FRLayout<>(graph.getGraph());
-		visualize(layout);
+		return layout;
 	}
 
 	/**
 	 * Visualize graph (A simple force-directed spring-embedder)
+         * @return layout
 	 */
-	public void SpringGraphVisualisation() {
+	public Layout<Integer, String> SpringGraphVisualisation() {
 		Layout<Integer, String> layout = new SpringLayout<>(graph.getGraph());
-		visualize(layout);
+		return layout;
 	}
 
 	/**
 	 * Visualize graph (Meyer's "Self-Organizing Map" layout)
+         * @return layout
 	 */
-	public void ISOMLGraphVisualisation() {
+	public Layout<Integer, String> ISOMLGraphVisualisation() {
 		Layout<Integer, String> layout = new ISOMLayout<>(graph.getGraph());
-		visualize(layout);
+		return layout;
 	}
 }
