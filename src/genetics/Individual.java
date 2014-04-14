7fcdb4a1289fe0fd933a2ccb45823624b4b651da
wukat <wojciechksprk@gmail.com>
2014-04-15 01:58:58 +0200

    constructor and getter to new version added

--- src/genetics/Individual.java
+++ src/genetics/Individual.java
@@ -2,6 +2,7 @@ package genetics;
 
 import java.util.LinkedList;
 import java.util.Random;
+import exceptions.NoPossibilityToCreateIndividualWhichYoudLikeToCreateAndICannotChangeItSoIThrowAnExceptionAndIThinkYouWillLikeItBecauseIDontHaveAnythingBetterToDoItsOnly2OClockNightIsYoung;
 
 /**
  * @author Krzysztof Spytkowski
@@ -12,14 +13,15 @@ public class Individual implements Comparable<Individual> {
     private final int[] chromosome; // table of subgraph's vertices (0 - not exists, 1 - exists)
     private int activeGenesAmount; // amount of vertices in subgraph
     private double fitness; // shows how well individual is adopted in population
+    private int numberOfSubgraphs; // actual number of groups in chromosome
 
     /**
-     * Constructor - creates subgraph that has size of kCliqueSize - chooses appropriate amount of genes (vertices) and puts them into chromosome (table)
-     * 
-     * @param graphSize
-     *            - graph's size (amount of vertices)
-     * @param kCliqueSize
-     *            - k-clique size (amount of vertices)
+     * Constructor - creates subgraph that has size of kCliqueSize - chooses
+     * appropriate amount of genes (vertices) and puts them into chromosome
+     * (table)
+     *
+     * @param graphSize - graph's size (amount of vertices)
+     * @param kCliqueSize - k-clique size (amount of vertices)
      */
     public Individual(int graphSize, int kCliqueSize) {
         // wyjatki?!
@@ -47,11 +49,39 @@ public class Individual implements Comparable<Individual> {
 
     }
 
+    /* MY VERSION */
+    /* wukat */
+    /* please, don't really care about it, I can be serious */
+    /* sometimes */
+    /**
+     * Constructor - creates graph (maximum size), every index in chromosome is
+     * a vertex and value assigned to vertex indicates to subgraph which
+     * contains this vertex
+     *
+     * @param graphSize - graph's size (amount of vertices)
+     * @param numberOfGroups - number of subgraphs
+     * @throws
+     * NoPossibilityToCreateIndividualWhichYoudLikeToCreateAndICannotChangeItSoIThrowAnExceptionAndIThinkYouWillLikeItBecauseIDontHaveAnythingBetterToDoItsOnly2OClockNightIsYoung
+     * - excetion thrown when number of groups is bigger than graph size,
+     * because it doesn't really make sense
+     */
+    public Individual(int graphSize, int numberOfGroups, boolean nextVersion) throws NoPossibilityToCreateIndividualWhichYoudLikeToCreateAndICannotChangeItSoIThrowAnExceptionAndIThinkYouWillLikeItBecauseIDontHaveAnythingBetterToDoItsOnly2OClockNightIsYoung { // next version - added temporarily, only to change signature
+        //this.activeGenesAmount = 0; // not needed at all
+        if (numberOfGroups > graphSize) {// not nice, may couse some stupid things
+            throw new NoPossibilityToCreateIndividualWhichYoudLikeToCreateAndICannotChangeItSoIThrowAnExceptionAndIThinkYouWillLikeItBecauseIDontHaveAnythingBetterToDoItsOnly2OClockNightIsYoung("Number of groups too big");
+        }
+        numberOfSubgraphs = numberOfGroups;
+        chromosome = new int[graphSize];
+        Random rand = new Random();
+        for (int i = 0; i < graphSize; i++) {
+            chromosome[i] = rand.nextInt(numberOfGroups); // groups coded from 0 to numberOfGroups - 1
+        }
+    }
+
     /**
      * Copy constructor
-     * 
-     * @param i
-     *            - individual
+     *
+     * @param i - individual
      */
     public Individual(Individual i) {
         this.activeGenesAmount = i.getActiveGenesAmount();
@@ -61,10 +91,9 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Constructor - creates blank Individual (all genes are 0)
-     * 
-     * @param chromosomeLength
-     *            - amount of genes
-     * 
+     *
+     * @param chromosomeLength - amount of genes
+     *
      */
     public Individual(int chromosomeLength) {
         this.activeGenesAmount = 0;
@@ -74,7 +103,7 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Getter
-     * 
+     *
      * @return length of chromosome
      */
     public int getChromosomeLength() {
@@ -83,9 +112,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Getter
-     * 
-     * @param index
-     *            - index of gene
+     *
+     * @param index - index of gene
      * @return value of gene: 0 - not exists, 1 - exists
      */
     public int getValueOfGene(int geneIndex) {
@@ -94,7 +122,7 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Getter
-     * 
+     *
      * @return amount of active genes
      */
     public int getActiveGenesAmount() {
@@ -102,8 +130,17 @@ public class Individual implements Comparable<Individual> {
     }
 
     /**
+     * Getter of number of subgraphs (groups)
+     *
+     * @return number of groups
+     */
+    public int getNumberOfSubgraphs() {
+        return numberOfSubgraphs;
+    }
+
+    /**
      * Getter
-     * 
+     *
      * @return chromosome - table
      */
     public int[] getChromosome() {
@@ -112,9 +149,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Getter
-     * 
-     * @param geneIndex
-     *            - index of gene
+     *
+     * @param geneIndex - index of gene
      * @return value of gene
      */
     public int getGene(int geneIndex) {
@@ -123,7 +159,7 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Getter
-     * 
+     *
      * @return fitness
      */
     public double getFitness() {
@@ -132,9 +168,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Setter
-     * 
-     * @param fitness
-     *            - fitness
+     *
+     * @param fitness - fitness
      */
     public void setFitness(double fitness) {
         this.fitness = fitness;
@@ -142,9 +177,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Removes gene - makes 0 form 1 in chromosome
-     * 
-     * @param geneIndex
-     *            - index of gene to remove
+     *
+     * @param geneIndex - index of gene to remove
      */
     public void removeGene(int geneIndex) {
         chromosome[geneIndex] = 0;
@@ -153,9 +187,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Adds gene - makes 1 form 0 in chromosome
-     * 
-     * @param geneIndex
-     *            - index of gene to remove
+     *
+     * @param geneIndex - index of gene to remove
      */
     public void addGene(int geneIndex) {
         chromosome[geneIndex] = 1;
@@ -163,12 +196,10 @@ public class Individual implements Comparable<Individual> {
     }
 
     /**
-     * Sets new value to gene
-     * 
-     * @param geneIndex
-     *            - index of gene
-     * @param value
-     *            - value to set
+     * Sets new value to gene to use in byte coding case
+     *
+     * @param geneIndex - index of gene
+     * @param value - value to set
      */
     public void setGene(int geneIndex, int value) {
         if (chromosome[geneIndex] != value && value == 1) {
@@ -181,9 +212,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Inverses gene - makes 0 form 1 or 1 from 0 in chromosome
-     * 
-     * @param geneIndex
-     *            - index of gene to inverse
+     *
+     * @param geneIndex - index of gene to inverse
      */
     public void inverseGene(int geneIndex) {
         setGene(geneIndex, (chromosome[geneIndex] == 0) ? (byte) 1 : (byte) 0);
@@ -191,9 +221,8 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Compares two individuals - better individual has better fitness
-     * 
-     * @param i
-     *            - individual
+     *
+     * @param i - individual
      */
     @Override
     public int compareTo(Individual i) {
@@ -205,11 +234,9 @@ public class Individual implements Comparable<Individual> {
 
     /**
      * Checks if "second" individual is better than "first" individual
-     * 
-     * @param first
-     *            - first individual to compare to
-     * @param second
-     *            - second individual to compare to
+     *
+     * @param first - first individual to compare to
+     * @param second - second individual to compare to
      * @return true if second individual is better, false otherwise
      */
     public static Individual isBetter(Individual first, Individual second) {
