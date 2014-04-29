9b423fb07ebf8965a1d907535747021672fc4e03
wukat <wojciechksprk@gmail.com>
2014-04-29 14:02:19 +0200

    removed comment, TODO - remove clas, not necessary, may be in other place

--- src/genetics/Mutation.java
+++ src/genetics/Mutation.java
@@ -8,45 +8,6 @@ import java.util.Random;
  */
 public class Mutation {
 
-//    private double mutationProbability; // individuals' mutation probability
-
-//    /**
-//     * Constructor
-//     * 
-//     * @param mutationProbability
-//     *            - probability of mutation
-//     * @throws GeneticAlgorithmException
-//     */
-//    public Mutation(double mutationProbability) throws GeneticAlgorithmException {
-//        if (mutationProbability < 0 || mutationProbability > 1) {
-//            throw new GeneticAlgorithmException("Probability of mutation should be more than 0 and less than 1");
-//        }
-//        this.mutationProbability = mutationProbability;
-//    }
-
-//    /**
-//     * Getter
-//     * 
-//     * @return individuals' mutation probability
-//     */
-//    public double getMutationProbability() {
-//        return mutationProbability;
-//    }
-//
-//    /**
-//     * Setter
-//     * 
-//     * @param mutationProbability
-//     *            - individuals' mutation probability
-//     * @throws GeneticAlgorithmException
-//     */
-//    public void setMutationProbability(double mutationProbability) throws GeneticAlgorithmException {
-//        if (mutationProbability < 0 || mutationProbability > 1) {
-//            throw new GeneticAlgorithmException("Probability of mutation should be more than 0 and less than 1");
-//        }
-//        this.mutationProbability = mutationProbability;
-//    }
-
     /**
      * Makes small mutations among individuals (changes some genes in their chromosome)
      * 
