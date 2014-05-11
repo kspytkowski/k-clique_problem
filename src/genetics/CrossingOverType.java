c4d2486b964e2a0f9c872a22436b1719d7681f90
wukat <wojciechksprk@gmail.com>
2014-05-11 15:59:09 +0200

    getAtIndex added

--- src/genetics/CrossingOverType.java
+++ src/genetics/CrossingOverType.java
@@ -5,5 +5,31 @@ package genetics;
  * @date 6th April 2014
  */
 public enum CrossingOverType {
-    ONEPOINTWITHTWOCHILDREN, ONEPOINTWITHONECHILD, UNIFORMCROSSOVER, WEIGHTEDUNIFORMCROSSOVER, TWOPOINTSWITHTWOCHILDREN, TWOPOINTSWITHONECHILD
+
+    ONEPOINTWITHTWOCHILDREN, ONEPOINTWITHONECHILD, UNIFORMCROSSOVER, WEIGHTEDUNIFORMCROSSOVER, TWOPOINTSWITHTWOCHILDREN, TWOPOINTSWITHONECHILD;
+
+    /**
+     * Gets type at given index
+     *
+     * @param index
+     * @return crossing over type
+     */
+    public static CrossingOverType getAtIndex(int index) {
+        switch (index) {
+            case 0:
+                return ONEPOINTWITHTWOCHILDREN;
+            case 1:
+                return ONEPOINTWITHONECHILD;
+            case 2:
+                return UNIFORMCROSSOVER;
+            case 3:
+                return WEIGHTEDUNIFORMCROSSOVER;
+            case 4:
+                return TWOPOINTSWITHTWOCHILDREN;
+            case 5:
+                return TWOPOINTSWITHONECHILD;
+            default:
+                return ONEPOINTWITHTWOCHILDREN;
+        }
+    }
 }
--- src/genetics/SelectionType.java
+++ src/genetics/SelectionType.java
@@ -5,5 +5,24 @@ package genetics;
  * @date 1st May 2014
  */
 public enum SelectionType {
-    TOURNAMENTSELECTION, ROULETTEWHEELSELECTION, LINEARRANKINGSELECTION
+    TOURNAMENTSELECTION, ROULETTEWHEELSELECTION, LINEARRANKINGSELECTION;
+    
+    /**
+     * Gets type at given index.
+     * 
+     * @param index
+     * @return type of selection
+     */
+    public static SelectionType getAtIndex(int index) {
+        switch (index) {
+            case 0:
+                return TOURNAMENTSELECTION;
+            case 1:
+                return ROULETTEWHEELSELECTION;
+            case 2:
+                return LINEARRANKINGSELECTION;
+            default:
+                return ROULETTEWHEELSELECTION;
+        }
+    }
 }
